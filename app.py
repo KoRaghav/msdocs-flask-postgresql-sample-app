889303aaba099e7fc75ebdfdbd72fec8e1b12545
KoRaghav <106992952+KoRaghav@users.noreply.github.com>
2025-03-17 17:30:00 +0530

    auth

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 import os
 from datetime import datetime
-
+import requests
 from flask import Flask, redirect, render_template, request, send_from_directory, url_for
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
-
+from identity.flask import Auth
+import app_config
 
 app = Flask(__name__, static_folder='static')
 csrf = CSRFProtect(app)
+app.config.from_object(app_config)
+auth = Auth(
+    app,
+    authority=app.config["AUTHORITY"],
+    client_id=app.config["CLIENT_ID"],
+    client_credential=app.config["CLIENT_SECRET"],
+    redirect_uri=app.config["REDIRECT_URI"]
+)
 
 if 'WEBSITE_HOSTNAME' not in os.environ:
     # local development, where we'll use environment variables
@@ -29,47 +38,45 @@ migrate = Migrate(app, db)
 from models import Product, Review
 
 @app.route('/', methods=['GET'])
-def index():
+@auth.login_required
+def index(*, context):
     print('Request for index page received')
     products = Product.query.all()
-    return render_template('index.html', products=products)
+    return render_template('index.html', products=products, user=context['user'])
 
 @app.route('/<int:id>', methods=['GET'])
-def details(id):
+@auth.login_required
+def details(id, context):
     product = Product.query.where(Product.id == id).first()
     reviews = Review.query.where(Review.product == id)
-    return render_template('details.html', product=product, reviews=reviews)
+    return render_template('details.html', product=product, reviews=reviews, user=context['user'])
 
 @app.route('/create', methods=['GET'])
-def create_product():
+@auth.login_required
+def create_product(*, context):
     print('Request for add product page received')
-    return render_template('create_product.html')
+    return render_template('create_product.html', user=context['user'])
 
 @app.route('/add', methods=['POST'])
 @csrf.exempt
 def add_product():
-    try:
-        name = request.values.get('product_name')
-        description = request.values.get('description')
-    except (KeyError):
-        # Redisplay the question voting form.
-        return render_template('create_product.html', {
-            'error_message': "You must include a name and description",
-        })
-    else:
-        product = Product()
-        product.name = name
-        product.description = description
-        db.session.add(product)
-        db.session.commit()
+    name = request.values.get('product_name')
+    description = request.values.get('description')
 
-        return redirect(url_for('details', id=product.id))
+    product = Product()
+    product.name = name
+    product.description = description
+    db.session.add(product)
+    db.session.commit()
+
+    return redirect(url_for('details', id=product.id))
 
 @app.route('/review/<int:id>', methods=['POST'])
+@auth.login_required
 @csrf.exempt
-def add_review(id):
+def add_review(id, context):
     try:
-        user_name = request.values.get('user_name')
+        user_name = context['user'].get('name')
         rating = request.values.get('rating')
         review_text = request.values.get('review_text')
     except (KeyError):
---
+++ app_config.py
@@ -0,0 +1,7 @@
+import os
+
+AUTHORITY = os.getenv("AUTHORITY")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
+SESSION_TYPE = "filesystem"
